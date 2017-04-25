fc6df33fc441b43126bd3e123155230e13c53dd8
clesire <clesiderata@gmail.com>
2017-04-25 18:39:18 -0400

    comment

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+//needed to build an Angular component
+//add at least 2 meta data properties
+//selector: what Angular uses to locate
+//a custom HTML element & apply the component
 @Component({
   selector: 'app',
   template: '<h1>My App</h1>'
