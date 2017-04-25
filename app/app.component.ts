ae2198b7f142a8c8279a9d3f6dfaa20b29cf50a8
clesire <clesiderata@gmail.com>
2017-04-25 19:14:50 -0400

    comment

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
 
+// when Angular finds a match in the DOM for a componenet selector
+// it will inject the componenet markup into the DOM element 
+// that it found a match on
 @Component({
   selector: 'mw-app',
+  //template: `
+  //  <h1>My App</h1>
+  //  <p>extra paragraph</p>
+  //`,
   templateUrl: 'app/app.component.html'
 })
 export class AppComponent {}
--- app/main.ts
+++ app/main.ts
@@ -1,3 +1,7 @@
+//trageting browser
+//this function returns a platform object 
+//that has a bootstrap module function on it
+//used to bootstrap root module on the platform
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 
