6847ae44ed61442bf3f0b163b4ed79fe28db8970
clesire <clesiderata@gmail.com>
2017-04-26 16:43:26 -0400

    comment

--- app/media-item-list.component.ts
+++ app/media-item-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+//bring in activated route from the angular router scoped package
 import { ActivatedRoute } from '@angular/router';
 
 import { MediaItemService } from './media-item.service';
@@ -15,12 +16,25 @@ export class MediaItemListComponent {
 
   constructor(
     private mediaItemService: MediaItemService,
+    //activated route service has a properety named
+    //params that is an observable
     private activatedRoute: ActivatedRoute) {}
 
   ngOnInit() {
+    //We subscribed to the params observable
+    //unlike the HTTP response observable that was one and done
+    //i.e. would cause unsubscribe as soon as it delivered a payload
+    //this observable is not; up to you to unsubscribe
+    //we wanna unsubscribe in this component because 
+    //router outlet will be adding and removing this component as routes are changed
+    //ngOnDestroy: component life cycle hook
+    //but first get a handle to this subscription:paramsSubscription
     this.paramsSubscription = this.activatedRoute.params
       .subscribe(params => {
+
+        //normalization to the medium
         let medium = params['medium'];
+        //to avoid from mock API treating it as a medium type
         if(medium.toLowerCase() === 'all') {
           medium = '';
         }
