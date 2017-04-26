4a18711145089a49a54b132e09704c45fb86176a
clesire <clesiderata@gmail.com>
2017-04-25 21:17:04 -0400

    comment

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,6 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'mw-app',
   templateUrl: 'app/app.component.html',
+//styles: [`
+//    h1 { color: #fff; }
+//    `,`
+//    .description { color: green; }
+//`],
   styleUrls: ['app/app.component.css']
 })
 export class AppComponent {}
