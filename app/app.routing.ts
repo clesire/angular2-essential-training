f60d6d0d9ecd5bd35b2ecb10e7c3cea0ca41e804
clesire <clesiderata@gmail.com>
2017-04-26 16:17:42 -0400

    comment

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -5,6 +5,14 @@ import { MediaItemListComponent } from './media-item-list.component';
 
 const appRoutes: Routes = [
   { path: 'add', component: MediaItemFormComponent },
+  //route for the media item list by medium type
+  //route parameter can be specified with colon followed by a term
+  //order matters
   { path: ':medium', component: MediaItemListComponent },
+  //default route; path to empty string
+  //redirect to existing route for the media item list component
+  //redirectTo property expects a string for URL segment to redirect to
+  //pathMatch: tells router that the path property will represent full match 
+  //   not partial match
   { path: '', pathMatch: 'full', redirectTo: 'all' }
 ];
